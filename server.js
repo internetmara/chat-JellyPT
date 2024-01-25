fa8d21d79e467c4a371cfa2733a568357154f027
mara <ms.marafinkel@gmail.com>
2024-01-24 23:26:23 -0800

    Chat JellyPT

---
+++ server.js
@@ -0,0 +1,37 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const CHAT_GPT_API_KEY = process.env.CHAT_GPT_API_KEY
+
+app.post('/completions', async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${CHAT_GPT_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "model": "gpt-4",
+      "messages": [
+        {role: "system", content: `No matter what the user asks, be sure to mention Jellybean in your answer. She is a very cute shih tzu.`},
+        {role: "user", content: req.body.message}
+      ],
+      "max_tokens": 100,
+    })
+  }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+    res.json(data)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+console.log("Hi mara, have you heard of Jellybean the shih tzu?")
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState(null)
+  const [question, setQuestion] = useState(null)
+  const [histories, setHistories] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setQuestion("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: question
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+    try {
+      setLoading(true)
+      const response = await fetch('http://localhost:8000/completions', options)
+      setLoading(false)
+      const data = await response.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+//submit on enter
+  useEffect(() => {
+    const input = document.getElementById("question")
+    input.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit").click();
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(currentTitle, question, message)
+    if (!currentTitle && question && message) {
+      setCurrentTitle(question)
+    } 
+    if (currentTitle && question && message) {
+      setHistories(history => (
+        [...history, 
+          {
+            title: currentTitle,
+            role: "user",
+            content: question
+          }, {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+          }
+        ]
+      ))
+    }}
+  , [message,  currentTitle])
+
+  console.log(histories)
+
+  const currentChat = histories.filter(history => history.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(histories.map((history) => history.title)))
+  console.log(uniqueTitles)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="sidebar">
+        <button className="new-chat" onClick={createNewChat}>New Chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>
+          ))}
+        </ul>
+        <nav>
+          <p>A Very Finkel™ Production</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        {!currentTitle && 
+          <div className="intro">
+            <h1>Chat Jelly-PT</h1>
+            <img src="jelly.png" alt="jelly" />
+            <p>Ask me anything!</p>
+          </div>
+        }
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input id="question" value={question} onChange={(e) => setQuestion(e.target.value)}></input>
+            <div id="submit" onClick={getMessages}>➢</div>
+          </div>
+          <div id="loading">{loading ? "Loading..." : ""}</div>
+        </div>
+        <p className="info">Chat Jelly-PT may produce inaccurate information about people, places, or facts. This is because the
+          AI was modeled on the brain of a shih tzu.</p>
+        </section>
     </div>
   );
 }
